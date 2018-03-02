794bbf753276a55a73c7673f678f31b43ca1ded6
kyhrikard <rikard.carlsson@student.kyh.se>
2018-03-02 18:12:19 +0100

    Very tired

--- web/index.php
+++ web/index.php
@@ -1,59 +1,57 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="stylesheets/main.css">
-<link rel="shortcut icon" href="images/favicon.ico" />
-<title>NEST:R Leaderboard</title>
-</head>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
+        <link rel="shortcut icon" href="images/favicon.ico" />
+        <title>NEST:R Leaderboard</title>
+    </head>
 <body>
 
 <?php
+    $url = 'https://nestr-dev-backend.herokuapp.com/api/currentteamscore';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $teamStandings = json_decode($data);
 
-$url = 'https://nestr-dev-backend.herokuapp.com/api/currentteamscore';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close($ch);
-$teamStandings = json_decode($data);
+    $url = 'https://nestr-dev-backend.herokuapp.com/api/topplayers';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $topPlayers = json_decode($data);
 
-$url = 'https://nestr-dev-backend.herokuapp.com/api/topplayers';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close($ch);
-$topPlayers = json_decode($data);
+    $url = 'https://nestr-dev-backend.herokuapp.com/api/snatchhistory';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $snatchHistory = json_decode($data);
 
-$url = 'https://nestr-dev-backend.herokuapp.com/api/snatchhistory';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close($ch);
-$snatchHistory = json_decode($data);
-
-$url = 'https://nestr-dev-backend.herokuapp.com/api/nests';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$data = curl_exec($ch);
-curl_close($ch);
-$nests = json_decode($data);
-
+    $url = 'https://nestr-dev-backend.herokuapp.com/api/nests';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $nests = json_decode($data);
 ?>
 
 <header>
-<h1>NEST:R leaderboard</h1>
-<p>0 lines of JavaScript were used building this page, only PHP, which we know shit about</p>
-<h4><a target="_blank" href="https://nestr.surge.sh">Get your name on the leaderboard here!</a></h4>
+    <h1>NEST:R leaderboard</h1>
+    <p>0 lines of JavaScript were used building this page, only PHP, which we know shit about</p>
+    <h4><a target="_blank" href="https://nestr.surge.sh">Get your name on the leaderboard here!</a></h4>
 </header>
 
 <div class="score-div">
@@ -77,38 +75,21 @@ foreach ($teamStandings as $value) {
 }
 ?>
 </div>
+
 <div class="nests-out-there">
     <h2><?php echo count($nests); ?></h2>
     <p>nests are waiting to get snatched</p>
 </div>
-<main>
-<!-- <div class="nestr-table">
-<h2>Team Standings</h2>
-<table>
-<th>Team</th>
-<th>Score</th>
-<?php
-foreach ($teamStandings as $value) {
-    echo "<tr>";
-    echo "<td>";
-    echo "$value->name";
-    echo "</td>";
-    echo "<td>";
-    echo "$value->currentscore";
-    echo "</td>";
-    echo "</tr>";
-}
-?>
-</table>
-</div> -->
 
-<div class="nestr-table nestr-table-left">
-<h2>Top snatchers</h2>
-<table>
-<th>Rank</th>
-<th>Snatcher</th>
-<th>Team</th>
-<th>Snatches</th>
+<main>
+
+<div class="nestr-table">
+    <h2>Top snatchers</h2>
+    <table>
+        <th>Rank</th>
+        <th>Snatcher</th>
+        <th>Team</th>
+        <th>Snatches</th>
 <?php
 $topTen = array_slice($topPlayers, 0, 100);
 $rank = 1;
@@ -134,10 +115,10 @@ foreach ($topTen as $value) {
     echo "</tr>";
 }
 ?>
-</table>
+    </table>
 </div>
 
-<div class="nestr-table nestr-table-right">
+<div class="nestr-table">
 <h2>100 latest snatches</h2>
 <table>
 <th>When</th>
@@ -158,11 +139,12 @@ foreach ($topTen as $value) {
     echo "<td>";
     echo "$value->nestname";
     echo "</td>";
+    echo "</tr>";
 }
 ?>
 </table>
 </div>
-<main>
+</main>
 
 </body>
 </html>
