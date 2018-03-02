ef5e4a0a7f8238a51c95863888f6a32d36cb29ff
kyhrikard <rikard.carlsson@student.kyh.se>
2018-03-03 00:56:23 +0100

    Fonti fonts

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="stylesheets/main.css">
         <link rel="shortcut icon" href="images/favicon.ico" />
         <title>NEST:R Leaderboard</title>
